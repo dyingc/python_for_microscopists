4a581d2fb3043e39ed35e6384feedadc06998ee7
ying.dong <ying.dong@oracle.com>
2024-06-16 21:08:57 -0700

    Tiny update

--- 052c-UMAP.py
+++ 052c-UMAP.py
@@ -8,14 +8,15 @@ from mpl_toolkits.mplot3d import Axes3D
 n_samples = 1000
 n_features = 1536
 centers = 16
+reduced_dim = 3
 cluster_std = (np.random.rand(centers) + 0.5) * 10 - 4 # 生成随机的标准差: [1, 11)
 
 X, y = make_blobs(n_samples=n_samples, n_features=n_features, centers=centers, cluster_std=cluster_std, random_state=42)
 
 # 使用 UMAP 进行降维
 n_neighbors = 15  # 控制邻域大小
-umap_model = umap.UMAP(n_neighbors=n_neighbors, random_state=42, n_components=3)
-X_umap = umap_model.fit_transform(X)
+umap_model = umap.UMAP(n_neighbors=n_neighbors, random_state=42, n_components=reduced_dim)
+X_umap = umap_model.fit_transform(X) # X_umap.shape = (n_samples, reduced_dim)
 
 # 绘制降维结果
 fig = plt.figure()
